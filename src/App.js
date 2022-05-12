51f5deb15164963a514b755a9fb46f084ea0b874
Leobardo Pineda <leobapineda@gmail.com>
2022-05-11 21:34:14 -0500

    Feat: add old directory files

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { React, useState } from "react";
+import boxesData from "./components/Boxes";
+import "./App.css";
+import Box from "./components/Box";
+
+export default function App({ darkMode }) {
+  const [box, setBox] = useState(boxesData);
+
+  function toggle(id) {
+    let newbox = box.find((box) => box.id == id)
+    newbox.on = !newbox.on
+    console.log(newbox.on);
+    // ya tenemos el array, ahora lo podemos usar 
+    setBox((box) => {
+      console.log(box); 
+      return(
+        [...box]
+      )
+    })
+  }
+
+  const boxElements = box.map((boxItem) => {
+    return <Box 
+    key={boxItem.id} 
+    on={boxItem.on} 
+    id={boxItem.id} 
+    handleClick = {toggle}
+    />;
+  });
+
+  return (
+    <main>
+      <h1>Boxes will go here</h1>
+      {boxElements}
+    </main>
+  );
+}
+
+//es un array de objetos
---
+++ src/components/Box.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "../style/Box.css";
+
+export default function Box({ on, id, handleClick }) {
+  
+
+  const personalStyles = {
+    backgroundColor: on ? "#222222" : "#ffffff",
+  };
+
+  return (
+    <div onClick={()=> handleClick(id)} style={personalStyles} className="box"></div>
+  );
+}
