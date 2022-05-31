29a5c86c80fd959dc4906eb51d0c102dc1e287ac
Leobardo Pineda <leobapineda@gmail.com>
2022-05-30 21:25:53 -0500

    Style: add backgroundColor to each box using ternary operator

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import { React, useState } from "react";
 import boxesData from "./components/Boxes";
 import "./App.css";
 
-export default function App() {
+export default function App(props) {
   const [squares, setSquares] = useState(boxesData);
 
   const styles = {
-    backgroundColor: '#222',
-    color: 'white'
+    backgroundColor: props.darkMode ? '#222' : '#ccc'
   }
 
   const squareElements = squares.map((square) => {
