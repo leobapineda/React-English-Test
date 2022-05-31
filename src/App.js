dcbe11576e1f97e01dbb07a73999fac65ebc1f22
Leobardo Pineda <leobapineda@gmail.com>
2022-05-30 21:21:08 -0500

    Feat: add backgroundColor to each box using inline JSX syntax

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import boxesData from "./components/Boxes";
 import "./App.css";
 
 export default function App() {
-  
-  const [squares, setSquares] = useState(boxesData)
+  const [squares, setSquares] = useState(boxesData);
+
+  const styles = {
+    backgroundColor: '#222',
+    color: 'white'
+  }
 
   const squareElements = squares.map((square) => {
-    return(
-      <div className="box" key={square.id} >{square.id}</div>
-    )
-  })
+    return (
+      <div style={styles} className="box" key={square.id}>
+        {square.id}
+      </div>
+    );
+  });
 
-  return (
-    <main>
-      {squareElements}
-    </main>
-  )
+  return <main>{squareElements}</main>;
 }
-
