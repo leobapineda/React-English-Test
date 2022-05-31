673e3a2a12e317943648e8d64fda492c25a87f45
Leobardo Pineda <leobapineda@gmail.com>
2022-05-31 11:55:18 -0500

    Feat: modify 'on' box property using for loop(video method)

--- src/App.js
+++ src/App.js
@@ -7,18 +7,37 @@ export default function App(props) {
   const [squares, setSquares] = useState(boxesData);
 
   function toggle(id) {
-    console.log(squares);
-    let clickSquare = squares.find(item => item.id == id)
-    clickSquare.on = !clickSquare.on
-    console.log(squares);
+    // ------>>>>>> my method
+    // console.log(squares);
+    // let clickSquare = squares.find(item => item.id == id)
+    // clickSquare.on = !clickSquare.on
+    // console.log(squares);
 
-    // aqui tengo la duda de si estamos cambiando estado 
-    // no entiendo porque el cambio de on del elemento encontrado con find, modifica el mismo elemento dentro del array de la base de datos
-    // creo que es porque desde un inicio le estamos diciendo que encuentre el elemento dentro del array squares, entonces al cambiarlo lo cambia dentro del array squares, porque ahi es donde lo encontro. entonces al llamar el setSquares((squares) ya lo estamos llamando con el nuevo valor del array squares con el elemento encontrado modificado. 
-    setSquares((squares) => {
-     return(
-       [...squares]
-     )
+    // // aqui tengo la duda de si estamos cambiando estado 
+    // // no entiendo porque el cambio de on del elemento encontrado con find, modifica el mismo elemento dentro del array de la base de datos
+    // // creo que es porque desde un inicio le estamos diciendo que encuentre el elemento dentro del array squares, entonces al cambiarlo lo cambia dentro del array squares, porque ahi es donde lo encontro. entonces al llamar el setSquares((squares) ya lo estamos llamando con el nuevo valor del array squares con el elemento encontrado modificado. 
+    // setSquares((squares) => {
+    //  return(
+    //    [...squares]
+    //  )
+    // })
+
+    // ------>>>>>> video method
+    // here we are generating a new array each time we click on one, then we map through that new array and get the array display again, but with the square that was clicked change, because its on property changed.
+    setSquares(prevSquares => {
+      const newSquares = []
+        for(let i = 0; i< prevSquares.length; i++) {
+          let currentSquare = prevSquares[i]
+          if(currentSquare.id == id) {
+            let changeSquare = {
+              ...currentSquare, on : !currentSquare.on
+            }
+            newSquares.push(changeSquare)
+          } else {
+            newSquares.push(currentSquare) 
+          }
+        }
+      return newSquares
     })
   }
 
