8a97cb1432c3e6a77498f3d57342d0efb745c353
Leobardo Pineda <leobapineda@gmail.com>
2022-05-31 11:33:44 -0500

    Feat: modify 'on' box property using find method
    Using the find method on the array in the database, find the array element that is click and change its on property

--- src/App.js
+++ src/App.js
@@ -6,18 +6,29 @@ import Box from "./components/Box";
 export default function App(props) {
   const [squares, setSquares] = useState(boxesData);
 
-  function toggle() {
-    console.log('click');
+  function toggle(id) {
+    console.log(squares);
+    let clickSquare = squares.find(item => item.id == id)
+    clickSquare.on = !clickSquare.on
+    console.log(squares);
+
+    // aqui tengo la duda de si estamos cambiando estado 
+    // no entiendo porque el cambio de on del elemento encontrado con find, modifica el mismo elemento dentro del array de la base de datos
+    // creo que es porque desde un inicio le estamos diciendo que encuentre el elemento dentro del array squares, entonces al cambiarlo lo cambia dentro del array squares, porque ahi es donde lo encontro. entonces al llamar el setSquares((squares) ya lo estamos llamando con el nuevo valor del array squares con el elemento encontrado modificado. 
+    setSquares((squares) => {
+     return(
+       [...squares]
+     )
+    })
   }
-  
 
   const squareElements = squares.map((square) => {
-
 
     return (
       <Box 
       key={square.id}
       on = {square.on}  
+      id = {square.id}  
       handleClick = {toggle}
       />
     );
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -2,15 +2,12 @@ import React, { useState } from "react";
 import "../style/Box.css";
 
 export default function Box({id, on, handleClick}) {
-  
-
-  
 
   const styles = {
     backgroundColor: on ? '#333' : 'transparent'
   }
  
   return(
-      <div onClick={handleClick} style={styles} className="box"></div>
+      <div onClick={() => {handleClick(id)}} style={styles} className="box"></div>
   )
 }
