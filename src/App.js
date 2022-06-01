ddd9122c745e3ead2a74b60c12236d44959450d9
Leobardo Pineda <leobapineda@gmail.com>
2022-06-01 11:29:11 -0500

    Feat(Form): add Form elements

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
 import { React, useState } from "react";
-import boxesData from "./components/Boxes";
 import "./App.css";
-import Box from "./components/Box";
+import Form from "./components/Form";
 
 export default function App(props) {
-  const [squares, setSquares] = useState(boxesData);
-
-  function toggle(id) {
-    setSquares((prevSquare) => {
-      return prevSquare.map((square) => {
-        return square.id == id ? { ...square, on: !square.on } : square;
-      });
-    });
-  }
-
-  const squareElements = squares.map((square) => {
-    return (
-      <Box
-        key={square.id}
-        on={square.on}
-        handleClick={() => toggle(square.id)}
-      />
-    );
-  });
-
-  return <main>{squareElements}</main>;
+  return (
+      <Form />
+  )
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Form() {
+    return (
+        <form>
+          <div>Form</div>
+            <input
+                type="text"
+                placeholder="First Name"
+            />
+        </form>
+    )
+}
