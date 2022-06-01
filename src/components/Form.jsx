5f93f06e32a5c3eb0af42545fd995dd1aabf20fc
Leobardo Pineda <leobapineda@gmail.com>
2022-06-01 13:29:16 -0500

    Test(Form): add radio and select input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,11 @@
+import { toBeChecked } from "@testing-library/jest-dom/dist/matchers"
 import React, { useState } from "react"
 
 export default function Form() {
   
-  const [name, setName] = useState('lol chan')
+  const [name, setName] = useState('')
+  const [flavor, setFlavor] = useState('')
+  const [language, setLanguage] = useState('')
 
 
 
@@ -19,8 +22,45 @@ export default function Form() {
          onChange={(e) => {
            setName(e.target.value)
          }}/>
+         <br />
+         <hr />
+         <div>Favorite JS flavor</div>
+         <input type="radio" name="sabor" id="vanilla" value='vanilla' defaultChecked onChange={(e) => {
+           setFlavor(e.target.value) 
+          }}/>
+          <label htmlFor="React">Vanilla</label>
+         <br />
+         <input type="radio" name="sabor" id="react" value='react'  onChange={(e) => {
+           setFlavor(e.target.value)
+          }}/>
+          <label htmlFor="React">React</label>
+         <br />
+         <input type="radio" name="sabor" id="vue" value='vue' onChange={(e) => {
+           setFlavor(e.target.value)
+          }}/>
+          <label htmlFor="React">Vue</label>
+         <br />
+
+         <input type="radio" name="sabor" id="other" value='other' onChange={(e) => {
+           setFlavor(e.target.value)
+          }}/>
+          <label htmlFor="React">Other</label>
        </form>
-       <div>{name}</div>
+       <br /><hr />
+          <p>Elije programa fav</p>
+          <select
+          name="language"
+          onChange={(e) => {
+            setLanguage(e.target.value)
+          }}
+          >
+            <option value='---' >---</option>
+            <option value='js' >js</option>
+            <option value='java' >java</option>
+            <option value='php'>php</option>
+            <option value='ruby' >ruby</option>
+          </select>
+
        </>
     )
 }
