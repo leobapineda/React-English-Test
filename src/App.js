1f18e70be694f6859b8ea03862d43b3b5be5bebe
Leobardo Pineda <leobapineda@gmail.com>
2022-06-02 12:40:20 -0500

    Test(API): add API element

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { React, useState } from "react";
 import "./App.css";
-import Form from "./components/Form";
+import Form from "./components/API";
 
 export default function App(props) {
   return (
