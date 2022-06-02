37888f65c426b685c416870d99c43581bb9e2008
Leobardo Pineda <leobapineda@gmail.com>
2022-06-02 13:05:56 -0500

    Test(API): add fetch method to API element

--- src/components/API.jsx
+++ src/components/API.jsx
@@ -1,11 +1,16 @@
 import React from "react"
 
 export default function App() {
-    const [data, setData] = React.useState({})
+    const [starWardata, setStarWardata] = React.useState({})
+    console.log('api');
+    fetch('https://swapi.dev/api/people/20')
+    .then(response => response.json())
+    .then(data => console.log(data))
 
     return (
         <div>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
+            <pre>{JSON.stringify(starWardata, null, 2)}</pre>
         </div>
     )
 }
+ 
