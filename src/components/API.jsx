9a9c6390637163639a56422a9b9d6fe313f17528
Leobardo Pineda <leobapineda@gmail.com>
2022-06-02 13:36:54 -0500

    Test(useEffect WDS): add useEffect to get type of data from API jsonplaceholder

--- src/components/API.jsx
+++ src/components/API.jsx
@@ -1,16 +1,28 @@
-import React from "react"
-
+import {React, useState, useEffect} from "react";
+import '../style/API.css'
 export default function App() {
-    const [starWardata, setStarWardata] = React.useState({})
-    console.log('api');
-    fetch('https://swapi.dev/api/people/20')
-    .then(response => response.json())
-    .then(data => console.log(data))
+    const [data, setData] =useState('users');
+    const [data1, setData1] =useState([]);
+    
 
-    return (
-        <div>
-            <pre>{JSON.stringify(starWardata, null, 2)}</pre>
-        </div>
-    )
+    // do some side effect when something happends
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${data}`)
+    .then((response) => response.json())
+    .then((json) => setData1(json));
+  }, [data])
+
+
+  return (
+    <div>
+      <button onClick={() => setData('users')} >users</button>
+      <button onClick={() => setData('posts')} >posts</button>
+      <button onClick={() => setData('comments')} >comments</button>
+        <h1>{data}</h1>
+        {data1.map((item) => {
+            return <div key={item.id}  className="json-item" >{JSON.stringify(item)}</div>
+            
+        }) }
+    </div>
+  );
 }
- 
