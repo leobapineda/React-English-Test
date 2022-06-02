550b5b785d926b2f6a80f64cd33508ad347a13d2
Leobardo Pineda <leobapineda@gmail.com>
2022-06-02 13:51:45 -0500

    Refactor(useEffect WDS): better syntax and add comments

--- src/components/API.jsx
+++ src/components/API.jsx
@@ -1,17 +1,23 @@
 import {React, useState, useEffect} from "react";
 import '../style/API.css'
 export default function App() {
-    const [data, setData] =useState('users');
+    const [data, setData] =useState('posts');
     const [data1, setData1] =useState([]);
     
-
+    console.log('lol'); //runs twice because the first time the data changes form nothing to post and the second because when it changes it renders again, but this time the data value of post does not change, so it stops and only renders twice
     // do some side effect when something happends
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/${data}`)
     .then((response) => response.json())
     .then((json) => setData1(json));
+    // only render when the value inside the [] change
   }, [data])
 
+    console.log('render');
+
+  useEffect(() => {
+    console.log('button click');
+}, [data])
 
   return (
     <div>
