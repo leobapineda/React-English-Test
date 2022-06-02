1e46eaa2250ad41ffbc34bd43ab2a73abeed4ef1
Leobardo Pineda <leobapineda@gmail.com>
2022-06-02 16:17:06 -0500

    Test (API): get new character on button click

--- src/components/API.jsx
+++ src/components/API.jsx
@@ -2,32 +2,25 @@ import { React, useState, useEffect } from "react";
 
 export default function App() {
   const [starWardata, setStarWardata] = useState({});
-  console.log("api");
+  const [count, setCount] = useState(1);
 
   function apiRequest() {
-      console.log('API request');
-    fetch("https://swapi.dev/api/people/1")
+    fetch(`https://swapi.dev/api/people/${count}`)
       .then((response) => response.json())
-      .then((data) => setStarWardata((starWardata) => (starWardata = data)
-      ));
+      .then((data) => setStarWardata((starWardata) => (starWardata = data)));
   }
-  useEffect(() => {
-    apiRequest()
-  }, []);
 
-  return ( 
-    <div> 
-      <pre>
-          
-          {JSON.stringify(starWardata, null, 2)}
-      
-      </pre>
+  useEffect(() => {
+    apiRequest();
+  }, [count]);
+
+  return (
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        Get next character{" "}
+      </button>
+      <div>{`the character number is ${count}`}</div>
+      <pre>{JSON.stringify(starWardata, null, 2)}</pre>
     </div>
   );
-}
-
-
-
-for(let i=1; i<= 100; i++ ){
-
 }
