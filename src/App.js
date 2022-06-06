47a9dc986eabf19e64eff19cfe69dff1a7c5f006
Leobardo Pineda <leobapineda@gmail.com>
2022-06-06 11:54:30 -0500

    Feat(recipe): fetch recipe with axios

--- src/App.js
+++ src/App.js
@@ -3,29 +3,49 @@ import axios from "axios";
 import Recipe from "./components/Recipe";
 export default function App() {
 
-  const YOUR_APP_ID = "1225815e";
-  const YOUR_APP_KEY = "e642963f4d2299d6ac085245011970ab";
-  // const food = 'milk'
+  // const YOUR_APP_ID = "1225815e";
+  // const YOUR_APP_KEY = "e642963f4d2299d6ac085245011970ab";
+
+   const YOUR_APP_ID = "a52b4d43";
+  const YOUR_APP_KEY = "e0e5c667605f5e91d8275c973531b80a";
+
+
 
   // the initial value of photos has to be something, otherwise the first render will throw and error because it is trying to use map on an undefined element
   const [recipeName, setRecipeName] = useState([]);
   const [food, setFood] = useState('')
   const [submit, setSubmit] = useState('')
+  const [generateKey, setGenerateKey] = useState(1)
+
   const url2 = "/api/recipes/v2?type=public&q=chicken&app_id=1225815e&app_key=e642963f4d2299d6ac085245011970ab&diet=balanced&random=false"
 
   const url3 = `https://api.edamam.com/api/recipes/v2?type=public&q=${food}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&diet=balanced&random=false`
 
  useEffect(() => {
-  fetch(`${url3}`)
-  .then(res => res.json())
-  .then(data => setRecipeName(data.hits))
+  axios.get(`${url3}`)
+  // .then(res => res.json())
+  .then(APIdata => {
+    setRecipeName(APIdata.data.hits)
+  })
  }, [submit])
 
  let map = recipeName.map(item => {
+
+   let recipeItem = item.recipe.ingredients.map(element => {
+      
+      return(
+        <li key={element.text} >{element.text}</li>
+      )
+    })
+    
+
    return(
-      <div key={item.recipe.label} >
+      <div key={item.recipe.calories} >
         <div>{item.recipe.label}</div>
         <img src={item.recipe.image}/>
+        <ul>
+          {recipeItem}
+        </ul>
       </div>
    )
  })
@@ -39,16 +59,13 @@ export default function App() {
     setSubmit(food)
   }
 
-  // al dar click al btn, cambiar el valor de la comida y volver a buscar
-  console.log(food);
   return (
     <>
-
     <form onSubmit={hanbleSubmit} >
       <label id="food" >Choose food</label>
       <input type='text'
       name="food input"
-      placeholder="Choose a food ingriend"
+      placeholder="Find a recipe"
       className="form--input"
       onChange={handleChange}
       defaultValue={food}
@@ -57,6 +74,8 @@ export default function App() {
 <button>Search</button>
     </form>
 
+    <button>Pizza</button>
+    <button>Pizza</button>
     <div>
         {map}
     </div>
