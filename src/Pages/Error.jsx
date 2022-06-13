c933940b74cfa9ec578556835f53618349f913dc
Leobardo Pineda <leobapineda@gmail.com>
2022-06-13 16:11:13 -0500

    Style(router): modify 404 page style for better UI

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom'
 function Error() {
   return (
     <div>
-      <h1>error page</h1>
+      <h1>404</h1>
+      <p>error page</p>
       <Link className='btn' to='/' >go home</Link>
     </div>
   )
