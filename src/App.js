100ec40e6355053aac8d1b7badc76e7d828f70ee
Leobardo Pineda <leobapineda@gmail.com>
2022-06-13 11:35:23 -0500

    Feat(router): import router components in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return(
     <>
-    <div>
-      lol
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"  element={
+          <div>home page</div>
+        }/>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
