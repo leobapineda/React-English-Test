5d87f660190ccde95f2b9a8ba7e5f13b1d433d7a
Leobardo Pineda <leobapineda@gmail.com>
2022-06-15 21:05:13 -0500

    Style(routerV2): add navbar style

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import About from "./About";
 
 function Home() {
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../stylesheet/Navbar.css"
+
 function Navbar() {
   return (
-    <>
-    <Link className="btn"  to='/' >go home</Link>
-      <Link className="btn" to="/about">
-        go about
-      </Link>
-      <Link className="btn" to="/products">
-        go products
-      </Link>
-    </>
+    <div className="navbar__container" >
+      <ul className="navbar__ul" >
+        <li className="btn" ><Link className="nav__link" to='/' >go home</Link></li>
+        <li className="btn" ><Link className="nav__link" to="/about">go about</Link></li>
+        <li className="btn" ><Link className="nav__link" to="/products">go products</Link></li>
+      </ul>
+    </div>
   );
 }
 
