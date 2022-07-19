a304caf8eff3882576c13b9b92c968e733a1be4b
Leobardo Pineda <leobapineda@gmail.com>
2022-07-18 21:03:36 -0500

    Feat: render books mapping books array and add state to show add to cart msg

--- test2/t1/first-app/src/App.js
+++ test2/t1/first-app/src/App.js
@@ -1,49 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import "./stylesheets/App.css";
+
 function App() {
+  const [isAdded, setIsAddedisAdded] = useState(false);
+  const [bookTitle, setBookTitle] = useState("");
+
+  const bookList = [
+    {
+      title: "The Vanishing Half: A Novel",
+      img: "https://m.media-amazon.com/images/I/71cS8NuKl6L._AC_UL320_.jpg",
+      author: "Brit Bennett",
+      price: 199,
+    },
+    {
+      title: "Laurie (Flash Relatos)",
+      img: "https://m.media-amazon.com/images/I/71xIOPc+v8L._AC_UL320_.jpg",
+      author: "Hernández Sendin",
+      price: 0,
+    },
+    {
+      title:
+        "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+      img: "https://m.media-amazon.com/images/I/81wgcld4wxL._AC_UL320_.jpg",
+      author: "James Clear",
+      price: 199,
+    },
+  ];
+
+  function addToCart(title) {
+    setIsAddedisAdded((prevState) => !prevState);
+    setBookTitle(title);
+    setTimeout(() => {
+      setIsAddedisAdded((prevState) => !prevState);
+    }, 1000);
+  }
+
+  const booksMap = bookList.map((singleBook) => {
+    return <Book addToCart={addToCart} key={singleBook.img} {...singleBook} />;
+  });
   return (
     <React.Fragment>
-      <Book
-        title={"la vida en la granja"}
-        img={"https://m.media-amazon.com/images/I/81wgcld4wxL._AC_UL320_.jpg"}
-        author={"lola la vaca"}
-      />
-      <Book
-        title={"caperucita roja"}
-        img={"https://m.media-amazon.com/images/I/71cS8NuKl6L._AC_UL320_.jpg"}
-        author={"el lobo"}
-      />
-      <Book
-        title={"pinocho"}
-        img={"https://m.media-amazon.com/images/I/71xIOPc+v8L._AC_UL320_.jpg"}
-        author={"GrillO loCo"}
-      />
+      <div className="book-list ">{booksMap}</div>
+      <BookAdded isAdded={isAdded} title={bookTitle} />
     </React.Fragment>
   );
 }
 
-function Book({ title, img, author }) {
+function Book({ title, img, author, price, addToCart }) {
   return (
     <article className="book-container">
       <div className="image-container">
-        <Image imgCover={img} />
+        <img src={img} alt="book cover" />
       </div>
-      <BookTitle title={title} />
-      <BookAuthor author={author} />
+      <h3>{title}</h3>
+      <p>{author}</p>
+      <p>{price}</p>
+      <button onClick={() => addToCart(title)} className="btn">
+        Add book to cart
+      </button>
     </article>
   );
 }
 
-function Image({ imgCover }) {
-  return <img className="book-image" src={imgCover} alt="book cover" />;
-}
-
-function BookTitle({ title }) {
-  return <h3 className="book-title">{title}</h3>;
-}
-
-function BookAuthor({ author }) {
-  return <p className="book-author">{author}</p>;
+function BookAdded({ isAdded, title }) {
+  return (
+    <h3 className={isAdded ? "add-book_msg added" : "add-book_msg"}>
+      {/* {`${title} added to card`} */}
+      {`${title} Book added to card`}
+    </h3>
+  );
 }
 
 export default App;
